fb9716b676be7e79075090ba22402d0262801b4e
maredi-yamini <161504218+maredi-yamini@users.noreply.github.com>
2026-01-04 06:15:44 +0530

    Update rag_pipeline.py
    Add medicine-name based drug selection logic

--- src/rag_pipeline.py
+++ src/rag_pipeline.py
@@ -1,18 +1,42 @@
 from src.fetch_data import fetch_drug_labels
 
-def answer_question(question):
-    records = fetch_drug_labels()
+def answer_question(question, medicine_name=None):
+    records = fetch_drug_labels(limit=10)
 
     if not records:
         return "No drug information available."
 
-    drug = records[0]
+    selected_drug = None
 
-    purpose = drug.get("indications_and_usage", ["Information not available"])[0]
-    warnings = drug.get("warnings", ["No warnings available"])[0]
+    if medicine_name:
+        for drug in records:
+            openfda = drug.get("openfda", {})
+            brand_names = openfda.get("brand_name", [])
+            generic_names = openfda.get("generic_name", [])
+
+            all_names = [name.lower() for name in brand_names + generic_names]
+
+            if medicine_name.lower() in all_names:
+                selected_drug = drug
+                break
+
+    if not selected_drug:
+        selected_drug = records[0]  # fallback
+
+    purpose = selected_drug.get(
+        "indications_and_usage",
+        ["Information not available"]
+    )[0]
+
+    warnings = selected_drug.get(
+        "warnings",
+        ["No warnings available"]
+    )[0]
 
     response = f"""
 Drug Information Summary:
+
+Medicine: {medicine_name if medicine_name else 'Sample Drug'}
 
 Purpose:
 {purpose}
